f0e3590dcd443fc22d843361d79f5881a86022ee
0xF16.eth <0xf16@protonmail.com>
2022-02-12 19:44:33 +0100

    adding types of cards

--- src/classes/Card.ts
+++ src/classes/Card.ts
@@ -1,4 +1,5 @@
-enum Type {
+export enum Type {
+    TRAP,
     FIRE,
     ICE,
     GOLD,
@@ -9,22 +10,39 @@ export default class Card {
     private _flipped: boolean;
     private _extracted: boolean;
 
-    private type: any; //TODO: add types
+    private _type: Type; //TODO: add types
     private quantity: number;
 
     constructor() {
-        this._flipped = !!Phaser.Math.Between(0, 1);
-        this._extracted = !!Phaser.Math.Between(0, 1);
+        this._flipped = false; //!!Phaser.Math.Between(0, 1);
+        this._extracted = false; //!!Phaser.Math.Between(0, 1);
 
-        this.type = Phaser.Math.Between(0, 4);
-        this.quantity = Phaser.Math.Between(0, 5);
+        this._type = randomEnum(Type); //Phaser.Math.Between(0, 4);
+        this.quantity = Phaser.Math.Between(1, 5);
     }
 
     public get flipped(): boolean {
         return this._flipped;
     }
 
+    public set flipped(value: boolean) {
+        this._flipped = value;
+    }
+
     public get extracted(): boolean {
         return this._extracted;
     }
+
+    public get type(): Type {
+        return this._type;
+    }
+}
+
+function randomEnum<T>(anEnum: T): T[keyof T] {
+    const enumValues = Object.keys(anEnum)
+      .map(n => Number.parseInt(n))
+      .filter(n => !Number.isNaN(n)) as unknown as T[keyof T][]
+    const randomIndex = Math.floor(Math.random() * enumValues.length)
+    const randomEnumValue = enumValues[randomIndex]
+    return randomEnumValue;
 }
